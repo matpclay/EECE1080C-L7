216a06682c9568749cde675433fb258bfc318bed
Matthew Clayton <claytomp@mail.uc.edu>
2016-11-07 19:18:44 -0500

    Task 2 Commit-Untested
    All Task 2 subtasks completed. None of them are tested.

--- string-stats-sandbox.cpp
+++ string-stats-sandbox.cpp
@@ -1,4 +1,4 @@
-// Author: <Your Name Here>
+// Author: Matthew Clayton
 // Source File: string-stats-sandbox.cpp
 // Description: Analyzes the provided file for statistics.
 
@@ -15,9 +15,9 @@ using namespace std;
 
 int main(int argc, char* argv[]){
   if(argc == 2){  // A parameter was given!
-  
+
     string file_string;
-    
+
     // Create a file handle and open it.
     ifstream inFile;
     inFile.open(argv[1]);
@@ -29,13 +29,13 @@ int main(int argc, char* argv[]){
     stringstream buffer;
     buffer << inFile.rdbuf();
     file_string = buffer.str();
-    
+
     // TODO: Fill me in. file_string contains the file.
-    
+
   }else{
     cout << "No input file given." << endl << endl;
   }
-  
+
 
   return 0;
 }
--- string-stats.cpp
+++ string-stats.cpp
@@ -1,4 +1,4 @@
-// Author: <fill in your name>
+// Author: Matthew Clayton
 // Source File: string-stats.cpp
 // Description: Various string statistics
 
@@ -7,39 +7,75 @@
 
 using namespace std;
 
+// returns number of lines
 unsigned numLines(const string& st){
-  // TODO: Fill me in.
-  return 0;
+  int lines = 0;
+  for (int i = 1; i < st.size(); i++)
+    if (st[i] == '\n')
+      lines++; // if new line character, increment
+  if (st[st.size() - 1] != '\n')
+    lines++; // if last character is not new line, add one
+  return lines;
 }
 
+// returns number of uppercase characters
 unsigned upperCaseChars(const string& st){
-  // TODO: Fill me in.
-  return 0;
+  int uppers = 0;
+  // for each character
+  for (int j = 0; j < st.size(); j++)
+    if(isupper(st[j])) // test if uppercase
+      uppers++; // if uppercase, increment counter
+  return uppers;
 }
 
+// returns number of lowercase characters
 unsigned lowerCaseChars(const string& st){
-  // TODO: Fill me in.
-  return 0;
+  int lowers = 0;
+  for (int k = 0; k < st.size(); k++)
+    if(islower(st[k]))
+      lowers++;
+  return lowers;
 }
 
+// returns number of punctuation marks
 unsigned numPunct(const string& st){
-  // TODO: Fill me in.
-  return 0;
+  int puncts = 0;
+  for (int m = 0; m < st.size(); m++)
+    if(ispunct(st[m]))
+      puncts++;
+  return puncts;
 }
 
+// returns number of decimal digits
 unsigned numDigits(const string& st){
-  // TODO: Fill me in.
-  return 0;
+  int digits = 0;
+  for (int n = 0; n < st.size(); n++)
+    if(isdigit(st[m]))
+      digits++;
+  return digits;
 }
 
 unsigned numWords(const string& st){
-  // TODO: Fill me in.
-  return 0;
+  int words = 0;
+  for (int p = 1; p < st.size(); p++)
+    if (isAlpha(st[p]) && !(isAlpha(st[p-1])))
+      words++; // if non-alpha appears before, must be a new word
+  if (isAlpha(st[0]))
+    words++; // if first character is alpha, must be a word at start
+  if (isAlpha(st[st.size()]) && isAlpha(st[st.size()-1]))
+`   // if last character is part of a word and previous
+    // character is part of same word, end must be a word
+    words++;
+
+  return words;
 }
 
 vector<int> characterCounts(const string& st){
   vector<int> ret(26);
   // TODO: Fill me in.
+  st.tolower(); // make everything lowercase to ease letter counting
+  for (int c = 0; c < st.size(); c++)
+    if (int(st[c]) >= 97 && int(st[c]) <= 122)
+      v[int(st[c]) - 97] +  = 1;
   return ret;
 }
-
